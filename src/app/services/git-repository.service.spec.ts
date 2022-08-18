85e7996997f8a9d9857b773b9e5091613adcc309
Leonardo Lyra <leobuosi09@hotmail.com>
2022-08-18 16:07:18 -0300

    Aprendizado - Implementação de teste unitário em angular

--- src/app/services/git-repository.service.spec.ts
+++ src/app/services/git-repository.service.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 import { GitRepositoryService } from './git-repository.service';
+import { HttpClient } from '@angular/common/http';
 
-describe('GitRepositoryService', () => {
+fdescribe('GitRepositoryService', () => {
   let service: GitRepositoryService;
+  let http: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(GitRepositoryService);
+    http = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Deve chamar um GET no endpoint correto', () => {
+    const spy = spyOn(http, 'get').and.callThrough();
+    service.listarRepos('');
+    expect(spy).toHaveBeenCalledWith('https://api.github.com/search/repositories?q=');
   });
 });
